50c3d15bf2e54a40bf13b28ced65f546f95dc99d
grenim1128 <grenim1128@gmail.com>
2020-07-22 12:17:38 +0900

    update AdminDAO.xml & IAdminDAO.java

--- src/com/amanne/biscuitball/mybatis/IAdminDAO.java
+++ src/com/amanne/biscuitball/mybatis/IAdminDAO.java
@@ -6,7 +6,7 @@
 
 package com.amanne.biscuitball.mybatis;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -40,7 +40,7 @@ public interface IAdminDAO
 
 	public int updateReportUserAnswer(ReportUserDTO reportUserDTO); // 회원신고답변수정
 
-	public int addReportUserResolve(ReportUserDTO reportUserDTO); // 회원신고처리
+	public void addReportUserResolve(ReportUserDTO reportUserDTO); // 회원신고처리
 
 	public int addReportUserPenalty(PenaltyDTO penaltyDTO); // (신고처리후)회원제재
 
@@ -56,7 +56,7 @@ public interface IAdminDAO
 
 	public int updateReportCrewAnswer(ReportCrewDTO reportCrewDTO); // 크루신고답변수정
 
-	public int addReportCrewResolve(ReportCrewDTO reportCrewDTO); // 크루신고처리
+	public void addReportCrewResolve(ReportCrewDTO reportCrewDTO); // 크루신고처리
 
 	public int addReportCrewPenalty(PenaltyDTO penaltyDTO); // 크루제재
 
@@ -72,7 +72,7 @@ public interface IAdminDAO
 
 	public int updateReportCrewPostAnswer(ReportCrewPostDTO reportCrewPostDTO); // 신고답변수정
 
-	public int addReportCrewPostResolve(ReportCrewPostDTO reportCrewPostDTO); // 신고처리
+	public void addReportCrewPostResolve(ReportCrewPostDTO reportCrewPostDTO); // 신고처리
 
 	public int addReportCrewPostPenalty(PenaltyDTO penaltyDTO); // 크루게시글제재
 
@@ -88,7 +88,7 @@ public interface IAdminDAO
 
 	public int updateReportCrewCommentAnswer(ReportCrewCommentDTO reportCommentDTO); // 신고답변수정
 
-	public int addReportCrewCommentResolve(ReportCrewCommentDTO reportCommentDTO); // 신고처리
+	public void addReportCrewCommentResolve(ReportCrewCommentDTO reportCommentDTO); // 신고처리
 
 	public int addReportCrewCommentPenalty(PenaltyDTO penaltyDTO); // 크루댓글제재
 
@@ -104,7 +104,7 @@ public interface IAdminDAO
 
 	public int updateReportMeetAnswer(ReportMeetDTO reportMeetDTO); // 신고답변수정
 
-	public int addReportMeetResolve(ReportMeetDTO reportMeetDTO); // 신고처리
+	public void addReportMeetResolve(ReportMeetDTO reportMeetDTO); // 신고처리
 
 	public int addReportMeetPenalty(PenaltyDTO penaltyDTO); // 모임제재
 
@@ -120,7 +120,7 @@ public interface IAdminDAO
 
 	public int updateReportMeetCommentAnswer(ReportMeetCommentDTO reportMeetCommentDTO); // 신고답변수정
 
-	public int addReportMeetCommentResolve(ReportMeetCommentDTO reportMeetCommentDTO); // 신고처리
+	public void addReportMeetCommentResolve(ReportMeetCommentDTO reportMeetCommentDTO); // 신고처리
 
 	public int addReportMeetCommentPenalty(PenaltyDTO penaltyDTO); // 모임댓글제재
 
@@ -136,7 +136,7 @@ public interface IAdminDAO
 
 	public int updateReportCourtNameAnswer(ReportCourtNameDTO reportCourtNameDTO); // 신고답변수정
 
-	public int addReportCourtNameResolve(ReportCourtNameDTO reportCourtNameDTO); // 신고처리
+	public void addReportCourtNameResolve(ReportCourtNameDTO reportCourtNameDTO); // 신고처리
 
 	public int addReportCourtNamePenalty(PenaltyDTO penaltyDTO); // 코트이름제재
 
@@ -152,7 +152,7 @@ public interface IAdminDAO
 
 	public int updateReportCourtReviewAnswer(ReportCourtReviewDTO reportCourtReviewDTO); // 신고답변수정
 
-	public int addReportCourtReviewResolve(ReportCourtReviewDTO reportCourtReviewDTO); // 신고처리
+	public void addReportCourtReviewResolve(ReportCourtReviewDTO reportCourtReviewDTO); // 신고처리
 
 	public int addReportCourtReviewPenalty(PenaltyDTO penaltyDTO); // 코트이름제재
 
@@ -168,43 +168,43 @@ public interface IAdminDAO
 
 	public int updateInquiryAnswer(InquiryDTO inquiryDTO); // 문의답변수정
 
-	public int addInquiryResolve(InquiryDTO inquiryDTO); // 문의처리
+	public void addInquiryResolve(InquiryDTO inquiryDTO); // 문의처리
 
 	public int removeInquiry(String inquiryCode); // 문의취소
 
-	public int makeUserPenalty(PenaltyDTO dto); // 관리자 회원 제재
+	public void makeUserPenalty(PenaltyDTO dto); // 관리자 회원 제재
 
-	public int makeCrewPenalty(PenaltyDTO dto); // 관리자 크루 제재
+	public void makeCrewPenalty(PenaltyDTO dto); // 관리자 크루 제재
 
-	public int makeCrewPostPenalty(PenaltyDTO dto); // 관리자 크루 게시판 제재
+	public void makeCrewPostPenalty(PenaltyDTO dto); // 관리자 크루 게시판 제재
 
-	public int makeCrewCommentPenalty(PenaltyDTO dto); // 관리자 크루 댓글 제재
+	public void makeCrewCommentPenalty(PenaltyDTO dto); // 관리자 크루 댓글 제재
 
-	public int makeMeetPenalty(PenaltyDTO dto); // 관리자 모임 제재
+	public void makeMeetPenalty(PenaltyDTO dto); // 관리자 모임 제재
 
-	public int makeMeetCommentPenalty(PenaltyDTO dto); // 관리자 모임 댓글 제재
+	public void makeMeetCommentPenalty(PenaltyDTO dto); // 관리자 모임 댓글 제재
 
-	public int makeCourtNamePenalty(PenaltyDTO dto); // 관리자 코트 이름 제재
+	public void makeCourtNamePenalty(PenaltyDTO dto); // 관리자 코트 이름 제재
 
-	public int makeCourtReviewPenalty(PenaltyDTO dto); // 관리자 코트 리뷰 제재
+	public void makeCourtReviewPenalty(PenaltyDTO dto); // 관리자 코트 리뷰 제재
 
-	public List<NoticeDTO> getNoticeList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 공지사항 조회
+	public ArrayList<NoticeDTO> getNoticeList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 공지사항 조회
 
 	public int countNotices(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue); // 공지사항 전체 수
 
 	public NoticeDTO getNotice(String noticeCode); // 특정 공지 조회
 
-	public List<FaqDTO> getFaqList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // FAQ 조회
+	public ArrayList<FaqDTO> getFaqList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // FAQ 조회
 
 	public int countFaqs(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue); // FAQ 전체 수
 
 	public FaqDTO getFaq(String faqCode); // 특정 FAQ 조회
 
-	public List<ReportTotalDTO> getReportList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 전체 신고 조회
+	public ArrayList<ReportTotalDTO> getReportList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 전체 신고 조회
 
 	public int countReports(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue); // 전체 신고 수
 
-	public List<InquiryDTO> getInquiryList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 전체 문의 조회
+	public ArrayList<InquiryDTO> getInquiryList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 전체 문의 조회
 
 	public int countInquiries(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue); // 전체 문의 수
 
@@ -225,4 +225,10 @@ public interface IAdminDAO
 	public ReportCourtReviewDTO getCourtReviewReport(String reportCode); // 특정 코트리뷰신고 조회
 
 	public ReportCourtNameDTO getCourtNameReport(String reportCode); // 특정 코트이름신고 조회
+	
+	public ArrayList<AdminDTO> getAdminList(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue, @Param("start") int start, @Param("end") int end); // 전체 관리자
+	
+	public int countAdmins(@Param("searchKey") String searchKey, @Param("searchValue") String searchValue); // 전체 관리자 수
+	
+	public AdminDTO getAdmin(String adminCode);
 }
