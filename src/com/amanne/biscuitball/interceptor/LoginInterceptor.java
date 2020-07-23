96b328706701ea941af1c53fa93c02431d2a0ae6
Jinnyeong Oh <wvite1004@gmail.com>
2020-07-23 20:06:22 +0900

    add LoginInterceptor.java

---
+++ src/com/amanne/biscuitball/interceptor/LoginInterceptor.java
@@ -0,0 +1,44 @@
+package com.amanne.biscuitball.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter
+{
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception
+	{
+		// TODO Auto-generated method stub
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception
+	{
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
+	{
+		String url = request.getRequestURL().toString();
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("adminInfo") == null && session.getAttribute("userInfo") == null)
+		{
+			//response.sendRedirect("/login");
+			//return false;
+		}
+		
+		return true;
+	}
+	
+}
