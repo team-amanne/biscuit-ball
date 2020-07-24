45486f9167b047e6f18c1288054426bc34c23864
Administrator <Administrator@DESKTOP-M7MM0KQ>
2020-07-24 18:21:23 +0900

    commit

--- src/com/amanne/biscuitball/controller/PlayController.java
+++ src/com/amanne/biscuitball/controller/PlayController.java
@@ -2,11 +2,17 @@ package com.amanne.biscuitball.controller;
 
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.amanne.biscuitball.model.PartyCreateModel;
+import com.amanne.biscuitball.model.UserInfo;
+import com.amanne.biscuitball.mybatis.UserDTO;
 
 @Controller
 @RequestMapping("/play")
@@ -15,6 +21,9 @@ public class PlayController
 
 	@Autowired
 	private HttpServletRequest request;
+	
+	@Autowired
+	private PartyCreateModel partyCreate;
 	
 	// 파티 선택으로 이동
 	@RequestMapping(value="/party/**", method=RequestMethod.GET)
@@ -33,8 +42,12 @@ public class PlayController
 	
 	// 파티 만들기 팝업
 	@RequestMapping("party/create")
-	public String playPartyCreate()
+	public String playPartyCreate(Model model, UserDTO userDto)
 	{
+		HttpSession session = request.getSession();
+		UserInfo userInfo = (UserInfo)session.getAttribute("userInfo");
+	
+		model.addAttribute("userDto", userDto);
 		return "/play/PlayParty_pu01";
 	}
 	
